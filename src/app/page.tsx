46ee7a7f4c86868225ac4139f17be645421d0679
Miguel Bizzi <miguel.bizzi@aglets.com.br>
2023-11-14 19:52:51 -0300

    finalizando abas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
+import { SettingsTabs } from "@/components/SettingsTabs";
 import Image from "next/image";
 
 export default function Home() {
-    return <h1>asdasd</h1>;
+    return (
+        <>
+            <h1 className="text-3xl font-medium text-zinc-900">Settings</h1>
+
+            <SettingsTabs />
+        </>
+    );
 }
